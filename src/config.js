797d0e79f7ab48880a157c389d4c2206f84c01a1
Jany Battung <janybattung@gmail.com>
2020-10-13 12:03:29 -0700

    update files

--- src/config.js
+++ src/config.js
@@ -1,3 +1,5 @@
 export default {
-    API_ENDPOINT: 'http://localhost:8000/api'
+    API_ENDPOINT: `https://peaceful-mountain-30858.herokuapp.com/api`,
+    // API_ENDPOINT: 'http://localhost:8000/api',
+    API_KEY: process.env.REACT_APP_API_KEY,
   }
